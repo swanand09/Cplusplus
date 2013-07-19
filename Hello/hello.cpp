3e8cbb2dbf073acc4cf58dcad62bac864caf71c2
swanand09 <admin@DEV-REDDY1.mediacall.local>
2013-07-19 14:57:55 +0400

    first commit chapter 0 hello

---
+++ Hello/hello.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   hello.cpp
+ * Author: admin
+ *
+ * Created on 19 juillet 2013, 14:21
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int howmany;
+    cin>>howmany;
+    while(howmany--> 0)
+        cout <<"Hello"<<endl;
+    return 0;
+}
+
